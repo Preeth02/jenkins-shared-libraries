9a04ac30823e9dd046d0c6b0af179ba50e18054c
Preetham <157118386+Preeth02@users.noreply.github.com>
2026-02-11 12:41:04 +0530

    Add Docker login and push commands

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -3,4 +3,5 @@ def call(String projectName, String imageTag){
                 sh "docker login -u ${env.USERNAME} -p ${env.PASSWORD}"
                 sh "docker image tag notes-app:latest ${env.USERNAME}/notes-app:latest"
                 sh "docker push ${env.USERNAME}/${projectName}:${imageTag}"
+   }
 }
